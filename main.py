bb0f75d5134f7403e1c1f3e160d36a3243c072ac
eyupkirci <eyupkirci@gmail.com>
2024-06-16 01:47:32 +0200

    feat: query parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Union
+from enum import Enum
 
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
@@ -6,6 +7,8 @@ from pydantic import BaseModel
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from typing_extensions import Annotated
 
 import json
 
@@ -25,16 +28,20 @@ def read_root():
     return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: Union[str, None] = None)->dict:
     return {"item_id": item_id, "q": q}
 
 @app.post("/items/{item_id}")
-def update_item(item_id: int, item: Item, q:Union[str, None]=None):
+def update_item(item_id: int, item: Annotated[Item, 'It is an Item class instance' ], q:Union[str, None]=None) -> dict:
     message = q
     return {"item_name": item.name, "item_id": item_id, "is_offer":item.is_offer, "message":message}
 
+@app.get("/items/{item_id}/new")
+def read_item(item_id: int, q1: Union[str, None] = None, q2: Union[str, None] = None,)->dict:
+    return {"item_id": item_id, "query1": q1, "query2": q2, }
+
 @app.get("/items/{item_id}/page", response_class=HTMLResponse)
-async def read_index(request: Request, item_id: int, item: Item, q:Union[str, None]=None):
+async def read_index(request: Request, item_id: int, item: Item, q: str | None = None):
     requestBody = await request.body()
     json_data = json.loads(requestBody)
     print(json_data)
