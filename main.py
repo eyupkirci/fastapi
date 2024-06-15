19b6b6abfe264903a86d6ab131b2ccffb9185006
eyupkirci <eyupkirci@gmail.com>
2024-06-16 00:10:35 +0200

    feat: add template page to post response usingJinja2Templates

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+import json
 
 
+# initiates new project
 app = FastAPI()
-
+#initiates template
+templates = Jinja2Templates(directory="templates")
 
 class Item(BaseModel):
-    name: str
+    name: str = "product"
     price: float
     is_offer: Union[bool, None] = None
-
 
 @app.get("/")
 def read_root():
@@ -22,7 +28,21 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-@app.put("/items/{item_id}")
+@app.post("/items/{item_id}")
 def update_item(item_id: int, item: Item, q:Union[str, None]=None):
     message = q
     return {"item_name": item.name, "item_id": item_id, "is_offer":item.is_offer, "message":message}
+
+@app.get("/items/{item_id}/page", response_class=HTMLResponse)
+async def read_index(request: Request, item_id: int, item: Item, q:Union[str, None]=None):
+    requestBody = await request.body()
+    json_data = json.loads(requestBody)
+    print(json_data)
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "item_id": item_id,
+        "item": item,
+        "message":q
+    })
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
